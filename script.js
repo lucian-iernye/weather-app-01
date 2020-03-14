8f7af8ca57ae02ff74c81c543d7bb9eb0504675c
Lucian Iernye <Lucian.Iernye@Nology.local>
2020-03-14 20:46:26 +0000

    background image added

--- script.js
+++ script.js
@@ -4,8 +4,64 @@ let units = "metric";
 
 let searchMethod;
 
-const searchWeather = searchTerm => {
-  fetch(
-    `http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}`
-  );
+const getSearchMethod = searchTerm => {
+  if (
+    searchTerm.length === 5 &&
+    Number.parseInt(searchTerm) + "" === searchTerm
+  ) {
+    searchMethod = "zip";
+  } else {
+    searchMethod = "q";
+  }
 };
+
+const searchWeather = searchTerm => {
+  getSearchMethod(searchTerm);
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`
+  )
+    .then(result => {
+      return result.json();
+    })
+    .then(result => {
+      init(result);
+    });
+};
+
+const init = resultFromServer => {
+  console.log(resultFromServer);
+
+  switch (resultFromServer.weather[0].main) {
+    case "Clear":
+      document.body.style.backgroundImage = 'url("img/clear.jpg")';
+      break;
+
+    case "Clouds":
+      document.body.style.backgroundImage = 'url("img/cloud.jpg")';
+      break;
+
+    case "Rain":
+    case "Drizzle":
+    case "Mist":
+      document.body.style.backgroundImage = 'url("img/rain.jpg")';
+      break;
+
+    case "Thunderstorm":
+      document.body.style.backgroundImage = 'url("img/storm.jpg")';
+      break;
+
+    case "Snow":
+      document.body.style.backgroundImage = 'url("img/snow.jpg")';
+      break;
+
+    default:
+      break;
+  }
+};
+
+document.getElementById("searchBtn").addEventListener("click", () => {
+  let searchTerm = document.getElementById("searchInput").value;
+  if (searchTerm) {
+    searchWeather(searchTerm);
+  }
+});
