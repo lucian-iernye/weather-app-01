1674bfc2271c062b3c5f0732928f62e70f3e3ba8
Lucian Iernye <lucian.iernye@gmail.com>
2020-03-26 22:27:27 +0000

    finished

--- script.js
+++ script.js
@@ -57,6 +57,48 @@ const init = resultFromServer => {
     default:
       break;
   }
+
+  let weatherDescriptionHeader = document.getElementById(
+    "weatherDescriptionHeader"
+  );
+  let temperatureElement = document.getElementById("temperature");
+  let humidityElement = document.getElementById("humidity");
+  let windSpeedElement = document.getElementById("windSpeed");
+  let cityHeader = document.getElementById("cityHeader");
+  let weatherIcon = document.getElementById("documentIconImg");
+
+  weatherIcon.src =
+    "http://openweathermap.org/img/w/" +
+    resultFromServer.weather[0].icon +
+    ".png";
+
+  let resultDescription = resultFromServer.weather[0].description;
+  weatherDescriptionHeader.innerText =
+    resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
+
+  temperatureElement.innerHTML =
+    Math.floor(resultFromServer.main.temp) + "&#176";
+
+  windSpeedElement.innerHTML =
+    "Winds at " + Math.floor(resultFromServer.wind.speed) + " m/s";
+
+  cityHeader.innerHTML = resultFromServer.name;
+  humidityElement.innerHTML =
+    "Humidity levels at " + resultFromServer.main.humidity + "%";
+
+  setPositionForWeatherInfo();
+};
+
+const setPositionForWeatherInfo = () => {
+  let weatherContainer = document.getElementById("weatherContainer");
+  let weatherContainerHeight = weatherContainer.clientHeight;
+  let weatherContainerWidth = weatherContainer.clientWidth;
+
+  weatherContainer.style.left = `calc(50% -${weatherContainerWidth / 2}px)`;
+
+  weatherContainer.style.top = `calc(50% -${weatherContainerHeight / 1.3}px)`;
+
+  weatherContainer.style.visibility = "visible";
 };
 
 document.getElementById("searchBtn").addEventListener("click", () => {
